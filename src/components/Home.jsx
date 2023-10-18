59b688a28b9fe61b4c01d13a3286721c61614a45
Gaurav Sharma <gauraxvx0410@gmail.com>
2023-10-19 04:03:22 +0530

    complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect,useState } from 'react';
 import logo from "../Images/bg1.png";
-import "../styles/home.css"
+import styles from "../styles/home.module.css"
 import Link from "react-scroll"
 import resume from "../Images/resume.pdf"
 
@@ -53,24 +53,24 @@ export const Home = () => {
       }
     }
   return (
-    <div id='home'>
-     <div className='left'>
+    <div id='home' className={styles.homes}>
+     <div className={styles.left}>
       <p>Hi, I'm</p>
       <h2  id="user-detail-name">Gaurav Sharma</h2>
       <img src='https://readme-typing-svg.demolab.com/?font=Fira+Code&size=42&duration=2000&pause=800&color=8E05C2&background=FFFFFF00&width=600&height=61&lines=Full+Stack+Web+Developer;Aspiring+MERN+Developer;Front+End+Developer' alt='animation' className='animation'/>
    <div>
 <a href='#contact'>
-<button className="buttonContact">
+<button className={styles.buttonContact}>
 Contact
  </button>
 </a>
 <a href={resume} download="Gaurav Sharma-Full Stack Web Developer (1).pdf" id="resume-link-1" target="_blank">
-<button  id="resume-button-2" onClick={handleresume}> Resume</button>
+<button  id="resume-button-2" className={styles.resumeButton} onClick={handleresume}> Resume</button>
     </a>
 </div>
 </div>
 <div className='right'>
- <img src={logo} className='background-img'/>
+ <img src={logo} className={styles.backgroundImg}/>
 </div>
      </div>
   )
