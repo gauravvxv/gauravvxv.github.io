dd9ba2f75fe22ff1cb0ac408228a05704e6c0884
Gaurav Sharma <gauraxvx0410@gmail.com>
2023-10-26 20:42:56 +0530

    resume added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
               <a
                 href={resume}
                 target="_blank"
-                download="Gaurav Sharma"
+                download="Gaurav Sharma.pdf"
                 className="nav-link resume"
                 id="resume-link-1"
               >
